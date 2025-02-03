d711f3864f400b558c815b97e0f4a8a09a88f0df
AlexanderMota <ba.mota@iesdoctorbalmis.com>
2025-02-03 20:04:09 +0100

    error commonModule

--- front_to_do_list_angular/src/app/app.component.ts
+++ front_to_do_list_angular/src/app/app.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { LoginFormComponent } from './components/login-form/login-form.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, LoginFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  boolFormLogIn: boolean = false;
   title = 'ToDo List';
-  abrirLogin(){
-    console.log('Abriendo login');
+  toggleFormLogIn(){
+    this.boolFormLogIn = !this.boolFormLogIn;
   }
 }
