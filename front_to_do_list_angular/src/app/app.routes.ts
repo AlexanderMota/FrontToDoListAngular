bd100ea44e30d7b1ce4d6066196ca44e805b0ff1
AlexanderMota <ba.mota@iesdoctorbalmis.com>
2026-06-23 20:17:02 +0200

    tarea id

--- front_to_do_list_angular/src/app/app.routes.ts
+++ front_to_do_list_angular/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LandingComponent } from './pages/landing/landing.component';
 import { PrivateComponent } from './pages/private/private.component';
 import { HomeComponent } from './pages/home/home.component';
+import { TareaComponent } from './pages/tarea/tarea.component';
 
 export const routes: Routes = [
   { path: '', component: LandingComponent }, // ✅ '/'
   { path: 'home', component: HomeComponent }, // ✅ actúa como Home
   { path: 'private', component: PrivateComponent }, 
+  { path: 'tarea/:id', component: TareaComponent }, 
   { path: '**', redirectTo: 'home', pathMatch: 'full' }, // ✅ Rutas incorrectas redirigen a '/home'
 ];
